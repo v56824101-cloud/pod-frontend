5f3359f0d6ebb4aa624228f707ecefa84d734607
v56824101-cloud <v56824101@gmail.com>
2026-07-20 19:35:25 +0800

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,5 @@
-// src/firebase.ts
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCGZsuzsiQ1mgpBULkQEqAr0ywi-ER91d4",
@@ -8,7 +7,7 @@ const firebaseConfig = {
   projectId: "mypod-shop",
   storageBucket: "mypod-shop.firebasestorage.app",
   messagingSenderId: "105727840696",
-  appId: "1:105727840696:web:d90be4cd3e1a285b927f4d"
+  appId: "1:105727840696:web:d90be4cd3e1a285b927f4d",
   measurementId: "G-PBRP9GE9TG"
 };
 
